9fb723eefff4dae33596dc40ea59f1b668abce13
unknown <fa51214kh@gmail.com>
2024-03-22 00:57:31 +0500

    second commit

---
+++ Famous Quote2/main.js
@@ -0,0 +1,8 @@
+"use strict";
+// Q:- 05
+// Storing the famous person's name in a variable called famous_person
+let famous_person = "SENECA";
+// Composing the message
+let message = `"We suffer more in imagination than in reality." - ${famous_person}`;
+// Printing the message
+console.log(message);
---
+++ Famous Quote2/main.ts
@@ -0,0 +1,10 @@
+// Q:- 05
+// Storing the famous person's name in a variable called famous_person
+let famous_person: string = "SENECA";
+
+// Composing the message
+let message: string = `"We suffer more in imagination than in reality." - ${famous_person}`;
+
+// Printing the message
+console.log(message);
+
